f4658a0753957f8cf6c6990e664dbf4edace3b4d
Evan Pratten <ewpratten@gmail.com>
2019-12-28 12:47:38 -0500

    Added Vector3 and some tests

---
+++ src/main/java/ca/retrylife/libvec/VecUtil.java
@@ -0,0 +1,18 @@
+package ca.retrylife.libvec;
+
+/**
+ * Utils for working with scalars and vectors
+ */
+public class VecUtil {
+
+    /**
+     * Returns value clamped between low and high boundaries.
+     *
+     * @param value Value to clamp.
+     * @param low   The lower boundary to which to clamp value.
+     * @param high  The higher boundary to which to clamp value.
+     */
+    public static double clamp(double value, double low, double high) {
+        return Math.max(low, Math.min(value, high));
+    }
+}
